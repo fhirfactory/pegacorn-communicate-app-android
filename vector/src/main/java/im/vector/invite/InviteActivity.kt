dcf7469e411c54da58a0b3076a8a95681f8dff00
Rafi Sadat <rafi.sadat@act.gov.au>
2020-09-08 17:27:35 +1000

    Invite Activity has been added.

---
+++ vector/src/main/java/im/vector/invite/InviteActivity.kt
@@ -0,0 +1,24 @@
+package im.vector.invite
+
+import androidx.lifecycle.ViewModelProviders
+import im.vector.R
+import im.vector.activity.SimpleFragmentActivity
+import im.vector.home.InviteRoomFragment
+
+class InviteActivity : SimpleFragmentActivity() {
+    override fun getTitleRes() = R.string.invite
+
+    private lateinit var viewModel: InviteActivityViewModel
+
+    override fun initUiAndData() {
+        super.initUiAndData()
+        viewModel = ViewModelProviders.of(this).get(InviteActivityViewModel::class.java)
+        viewModel.initSession(mSession)
+
+        if (supportFragmentManager.fragments.isEmpty()) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, InviteRoomFragment())
+                    .commitNow()
+        }
+    }
+}
