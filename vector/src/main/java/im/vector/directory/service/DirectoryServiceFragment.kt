9745201a621e7cf48d838a469bbe6238d0d1ec7a
Rafi Sadat <rafi.sadat@act.gov.au>
2020-10-02 14:09:40 +1000

    Service List in directory

--- vector/src/main/java/im/vector/directory/service/DirectoryServiceFragment.kt
+++ vector/src/main/java/im/vector/directory/service/DirectoryServiceFragment.kt
@@ -1,12 +1,20 @@
 package im.vector.directory.service
 
+import android.os.Bundle
 import android.view.Menu
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.SimpleItemAnimator
 import im.vector.R
 import im.vector.directory.BaseDirectoryFragment
+import kotlinx.android.synthetic.main.fragment_directory_service.*
 import org.matrix.androidsdk.data.Room
 
 
-class DirectoryServiceFragment : BaseDirectoryFragment() {
+class DirectoryServiceFragment : BaseDirectoryFragment(), ServiceClickListener {
+    private lateinit var viewModel: DirectoryServiceViewModel
+    private lateinit var serviceAdapter: ServiceDirectoryAdapter
+
     override fun getLayoutResId() = R.layout.fragment_directory_service
 
     override fun onPrepareOptionsMenu(menu: Menu) {
@@ -14,6 +22,8 @@ class DirectoryServiceFragment : BaseDirectoryFragment() {
     }
 
     override fun onFavorite(enable: Boolean) {
+        //Temporary
+        setHeader(header, if (enable) R.string.total_number_of_favourite_service else R.string.total_number_of_service, if (enable) 4 else 10)
     }
 
     override fun getRooms(): MutableList<Room> {
@@ -26,5 +36,32 @@ class DirectoryServiceFragment : BaseDirectoryFragment() {
 
     override fun onResetFilter() {
         TODO("Not yet implemented")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DirectoryServiceViewModel::class.java)
+
+        serviceAdapter = ServiceDirectoryAdapter(requireContext(), this)
+        (serviceRecyclerview.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+        serviceRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+        serviceRecyclerview.adapter = serviceAdapter
+        serviceRecyclerview.setHasFixedSize(true)
+
+        val testServiceData = mutableListOf<DummyService>()
+        testServiceData.add(DummyService("1", "Service name 1", "Service Telecom 1", "Service Address 1", "Service Organization 1", true));
+        testServiceData.add(DummyService("2", "Service name 2", "Service Telecom 1", "Service Address 1", "Service Organization 1", true));
+        testServiceData.add(DummyService("3", "Service name 3", "Service Telecom 1", "Service Address 1", "Service Organization 1", false));
+        testServiceData.add(DummyService("4", "Service name 4", "Service Telecom 1", "Service Address 1", "Service Organization 1", false));
+        testServiceData.add(DummyService("5", "Service name 5", "Service Telecom 1", "Service Address 1", "Service Organization 1", false));
+        testServiceData.add(DummyService("6", "Service name 6", "Service Telecom 1", "Service Address 1", "Service Organization 1", true));
+        testServiceData.add(DummyService("7", "Service name 7", "Service Telecom 1", "Service Address 1", "Service Organization 1", false));
+
+        serviceAdapter.setData(testServiceData)
+        setHeader(header, R.string.total_number_of_roles,10)
+    }
+
+    override fun onServiceClick(service: DummyService) {
+        // TODO("Not yet implemented")
     }
 }
