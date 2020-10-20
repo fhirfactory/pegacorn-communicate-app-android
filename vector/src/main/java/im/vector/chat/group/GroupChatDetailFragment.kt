bdfea0a221baa618a6fdc91ed04e9e1de63185d0
Rafi Sadat <rafi.sadat@act.gov.au>
2020-10-20 12:17:06 +1100

    group chat detail

--- vector/src/main/java/im/vector/chat/group/GroupChatDetailFragment.kt
+++ vector/src/main/java/im/vector/chat/group/GroupChatDetailFragment.kt
@@ -1,9 +1,21 @@
 package im.vector.chat.group
 
+import android.view.MenuItem
 import im.vector.R
 import im.vector.activity.MXCActionBarActivity
+import org.matrix.androidsdk.core.Log
 
 class GroupChatDetailFragment : MXCActionBarActivity(){
     override fun getLayoutRes() = R.layout.fragment_group_chat_create_detail
 
+    override fun getMenuRes() = R.menu.create
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            R.id.ic_action_create -> {
+                Log.d("zzzz", "Next")
+                true
+            }
+            else -> false
+        }
 }
