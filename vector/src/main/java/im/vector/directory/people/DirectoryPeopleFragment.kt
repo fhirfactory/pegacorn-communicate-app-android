518df6cad110155b7eed2b46e007412bebe28bb3
Rafi Sadat <rafi.sadat@act.gov.au>
2020-11-23 20:18:23 +1100

    forcefully online

--- vector/src/main/java/im/vector/directory/people/DirectoryPeopleFragment.kt
+++ vector/src/main/java/im/vector/directory/people/DirectoryPeopleFragment.kt
@@ -58,8 +58,8 @@ class DirectoryPeopleFragment : BaseDirectoryFragment(), PeopleClickListener {
         val testPeopleData = mutableListOf<DirectoryPeople>()
         testPeopleData.add(DirectoryPeople("1", "John Smith", "Registrar", null, "Emergency Department", "Hospital Department"))
         testPeopleData.add(DirectoryPeople("2", "Paul George", "Ward Nurse", null, "Emergency Department", "Hospital Department"))
-        testPeopleData.add(DirectoryPeople("3", "Anthony Davis", "Nursing Team Leader", null, "Emergency Department", "Hospital Department"))
-        testPeopleData.add(DirectoryPeople("4", "Stephen Curry", "Emergency Admitting Officer", null, "Emergency Department", "Hospital Department"))
+        testPeopleData.add(DirectoryPeople("3", "Anthony Davis", "Nurse", null, "Emergency Department", "Hospital Department"))
+        testPeopleData.add(DirectoryPeople("4", "Stephen Curry", "Emergency Consultant", null, "Emergency Department", "Hospital Department"))
         testPeopleData.add(DirectoryPeople("5", "James Harden", "Consultant", null, "Emergency Department", "Hospital Department"))
         testPeopleData.add(DirectoryPeople("6", "Mike Jones", "Consultant", null, "Emergency Department", "Hospital Department"))
 
